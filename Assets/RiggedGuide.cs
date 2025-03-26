546b49936cca7d55d27e3ca46d111d0776deac4e
Charlie Baker <138892841+cmaximb@users.noreply.github.com>
2025-03-26 03:12:44 +0000

    Added polycam following player

--- Assets/RiggedGuide.cs
+++ Assets/RiggedGuide.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RiggedGuide : MonoBehaviour
 {
     public Transform player;
     public float followSpeed = 1.0f;
     public float rotationSpeed = 5.0f;
-    public float stoppingDistance = 4.0f;
+    //public float stoppingDistance = 2f;
+    private NavMeshAgent agent;
+    private Animator movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        movement = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,13 +26,21 @@ public class RiggedGuide : MonoBehaviour
 
         float distance = Vector3.Distance(transform.position, player.position);
 
-        if (distance > stoppingDistance )
+        if (distance > agent.stoppingDistance - 0.5)
         {
-            Vector3 direction = (player.position - transform.position).normalized;
-            transform.position += direction * followSpeed * Time.deltaTime;
+            //Vector3 direction = (player.position - transform.position).normalized;
+            //transform.position += direction * followSpeed * Time.deltaTime;
+            agent.SetDestination(player.position);
+            movement.SetFloat("Speed", agent.velocity.magnitude);
 
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            //Quaternion targetRotation = Quaternion.LookRotation(direction);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //agent.SetDestination(transform.position);
+            agent.ResetPath();
+            movement.SetFloat("Speed", 0);
         }
     }
 }
